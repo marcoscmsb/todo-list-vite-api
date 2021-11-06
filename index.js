08005e9ddc717c2bfc72908fa3a0fad4c0f3bbdc
Marcos <marcoscmsb@outlook.com>
2021-11-06 16:52:49 -0400

    Implementação API

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 let express = require("express")
+let cors = require("cors")
+
+let tarefas = [
+    {
+        descricao: "Tirar o lixo",
+        data: new Date("2021-11-06"),
+        conculida: true
+    },
+    {
+        descricao: "Lavar louça",
+        data: new Date("2021-11-08"),
+        conculida: true
+    },
+    {
+        descricao: "Limpar casa",
+        data: new Date("2021-11-05"),
+        conculida: true
+    }
+]
 
 let api = express() // inicia a aplicação com express
 api.use(express.json()) // autoriza requisição com o body em JSON
+api.use(cors({
+    origin: "http://localhost:8080"
+})) 
+
 
 api.get("/", function(req, res){
     res.json({ mensagem: "Olá mundo"})
@@ -10,6 +33,15 @@ api.get("/", function(req, res){
 api.post("/", function(req, res){
     console.log(req.body)
     res.json({ mensagem: `A mensagem recebida foi: ${req.body.mensagem}`})
+})
+
+api.get("/tarefas", function(req, res) {
+    res.json(tarefas)
+})
+
+api.post("/tarefas", function(req, res){
+    tarefas.push(req.body)
+    res.json({ mensagem: "Adicionado como sucesso"})
 })
 
 api.listen(3000, function(){
